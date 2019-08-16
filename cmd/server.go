d6475bd2bb79306a11b6b8a37b05f9b2f7fdca72
mpppk <niboshiporipori@gmail.com>
2019-08-16 12:31:13 +0900

    Add endpoint for receive account activity

--- cmd/server.go
+++ cmd/server.go
@@ -4,8 +4,13 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/base64"
+	"fmt"
 	"net/http"
 	"os"
+
+	"github.com/labstack/echo/v4/middleware"
+
+	"github.com/ChimeraCoder/anaconda"
 
 	"github.com/mpppk/sutaba-server/internal/option"
 
@@ -23,6 +28,16 @@ type CRCResponse struct {
 	ResponseToken string `json:"response_token"`
 }
 
+type TweetCreateEvents struct {
+	ForUserId         string           `json:"for_user_id"`
+	TweetCreateEvents []anaconda.Tweet `json:"tweet_create_events"`
+}
+
+func bodyDumpHandler(c echo.Context, reqBody, resBody []byte) {
+	fmt.Printf("Request Body: %v\n", string(reqBody))
+	fmt.Printf("Response Body: %v\n", string(resBody))
+}
+
 func newServerCmd(fs afero.Fs) (*cobra.Command, error) {
 	cmd := &cobra.Command{
 		Use:   "server",
@@ -35,7 +50,10 @@ func newServerCmd(fs afero.Fs) (*cobra.Command, error) {
 			}
 
 			e := echo.New()
-			e.GET("/twitter/aaa", func(c echo.Context) error {
+			e.Use(middleware.BodyDump(bodyDumpHandler))
+
+			endpoint := "/twitter/aaa"
+			e.GET(endpoint, func(c echo.Context) error {
 				req := new(CRCRequest)
 				if err = c.Bind(req); err != nil {
 					return err
@@ -43,6 +61,20 @@ func newServerCmd(fs afero.Fs) (*cobra.Command, error) {
 				response := &CRCResponse{ResponseToken: CreateCRCToken(req.CRCToken, conf.TwitterConsumerSecret)}
 				return c.JSON(http.StatusOK, response)
 			})
+
+			e.POST(endpoint, func(c echo.Context) error {
+				events := new(TweetCreateEvents)
+				if err = c.Bind(events); err != nil {
+					return err
+				}
+				fmt.Printf("%#v\n", events)
+				if events.TweetCreateEvents == nil {
+					return c.NoContent(http.StatusNoContent)
+				}
+
+				return c.NoContent(http.StatusNoContent)
+			})
+
 			port := "1323"
 			envPort := os.Getenv("PORT")
 			if envPort != "" {
