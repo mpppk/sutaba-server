47765f1d53ee284ec93b061191f52881d11298ef
mpppk <niboshiporipori@gmail.com>
2019-08-28 13:59:46 +0900

    Update GCP scripts

--- scripts/gcp/staging_deploy_to_cloud_run.sh
+++ scripts/gcp/staging_deploy_to_cloud_run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-gcloud beta run deploy sutaba-server \
-    --image asia.gcr.io/sutaba/sutaba-server \
+gcloud beta run deploy sutaba-staging-server \
+    --image asia.gcr.io/sutaba/sutaba-server:master \
     --platform managed \
     --region asia-northeast1 \
     --update-env-vars CLASSIFIER_SERVER_HOST=https://classifier-server-lkui2qyzba-an.a.run.app \
