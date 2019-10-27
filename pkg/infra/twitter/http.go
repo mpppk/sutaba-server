7f403eb561fb0033015e17b4d5cdef0a0bb62740
mpppk <niboshiporipori@gmail.com>
2019-10-27 20:21:01 +0900

    Use anacondaaaa

--- pkg/infra/twitter/http.go
+++ pkg/infra/twitter/http.go
@@ -4,15 +4,16 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/mpppk/anacondaaaa"
 )
 
 func GenerateCRCTestHandler(twitterConsumerSecret string) func(c echo.Context) error {
 	return func(c echo.Context) error {
-		req := new(CRCRequest)
+		req := new(anacondaaaa.CRCRequest)
 		if err := c.Bind(req); err != nil {
 			return err
 		}
-		response := &CRCResponse{ResponseToken: CreateCRCToken(req.CRCToken, twitterConsumerSecret)}
+		response := &anacondaaaa.CRCResponse{ResponseToken: CreateCRCToken(req.CRCToken, twitterConsumerSecret)}
 		return c.JSON(http.StatusOK, response)
 	}
 }
