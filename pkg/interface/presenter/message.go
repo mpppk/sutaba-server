1706c77af80f4b6b9c80e1880cc3f53efde64680
mpppk <niboshiporipori@gmail.com>
2019-10-29 22:39:16 +0900

    Add test for messagen

--- pkg/interface/presenter/message.go
+++ pkg/interface/presenter/message.go
@@ -2,9 +2,7 @@ package presenter
 
 import (
 	"fmt"
-	"math/rand"
 	"strings"
-	"time"
 
 	"golang.org/x/xerrors"
 
@@ -16,9 +14,11 @@ type MessagenType string
 
 const (
 	RootType              = "Root"
+	BeginningMessageType  = "BeginningMessage"
 	TweetCheckType        = "TweetCheck"
 	SutabaDescriptionType = "SutabaDescription"
 	GoodEmojiType         = "GoodEmoji"
+	BadEmojiType          = "BadEmoji"
 	LastMessageType       = "LastMessage"
 	ExclamationType       = "Exclamation"
 	ThinkingEmojiType     = "ThinkingEmoji"
@@ -34,6 +34,8 @@ const (
 	DebugOnValue          = "on"
 	DebugOffValue         = "off"
 	RuleNumType           = "RuleNum"
+	TargetNameType        = "TargetName"
+	RamenSuffixType       = "RamenSuffix"
 )
 
 func generateResultMessage(result *domain.ClassifyResult) (string, error) {
@@ -48,20 +50,18 @@ func generateResultMessage(result *domain.ClassifyResult) (string, error) {
 	}
 
 	state := map[string]string{
-		"Class": result.Class,
+		ClassType: result.Class,
 	}
 
 	if confidence > 0.8 {
-		state["Confidence"] = "High"
+		state[ConfidenceType] = ConfidenceHighValue
 	} else if confidence > 0.5 {
-		state["Confidence"] = "Medium"
+		state[ConfidenceType] = ConfidenceMediumValue
 	} else {
-		state["Confidence"] = "Low"
+		state[ConfidenceType] = ConfidenceLowValue
 	}
 
-	fmt.Println("state", state)
-
-	messages, err := generator.Generate("Root", state, 1)
+	messages, err := generator.Generate(RootType, state, 1)
 	if err != nil {
 		return "", err
 	}
@@ -89,26 +89,101 @@ func getMessagenDefinitions() []*messagen.Definition {
 		{
 			Type: RootType,
 			Templates: []string{
-				toTemplate("ピピーッ❗️🔔⚡️スタバ警察です❗️👊👮❗️\n%s%sスタバ❗️❗️%s\n%s",
-					TweetCheckType, SutabaDescriptionType, GoodEmojiType, LastMessageType),
+				w(BeginningMessageType) +
+					w(TweetCheckType) + w(SutabaDescriptionType) + "スタバ❗️❗️" + w(GoodEmojiType) + "\n" +
+					w(LastMessageType),
 			},
 			Constraints: map[string]string{ClassType: ClassSutabaValue},
 		},
 		{
 			Type: RootType,
-			Templates: []string{toTemplate("ピピーッ❗️🔔⚡️スタバ警察です❗️👊👮❗️\n"+
-				"アナタのツイート💕は❌スタバ法❌第%s条🙋\n"+
-				"「スタバぢゃないツイートをスタバなうツイート💕してゎイケナイ❗️」\n"+
-				"に違反しています😡今スグ消しなｻｲ❗️❗️❗️❗️✌️👮🔫\n", RuleNumType)},
+			Templates: []string{
+				w(BeginningMessageType) +
+					w(TweetCheckType) + w(SutabaDescriptionType) + "ラーメン❗️❗️" + w(GoodEmojiType) + "\n" +
+					"ズルズルズルズル❗❗️❗️❗" + w(LastMessageType),
+			},
 			Constraints: map[string]string{
-				ClassType:            ClassSutabaValue,
+				ClassType:            ClassRamenValue,
+				ConfidenceType + "/": ConfidenceHighValue + "|" + ConfidenceMediumValue,
+			},
+		},
+		{
+			Type: RootType,
+			Templates: []string{
+				toTemplate("%sアナタのツイート💕は❌スタバ法❌第%s条🙋"+
+					"「%sをスタバなうツイート💕してゎイケナイ❗️」"+
+					"に違反しています😡今スグ消しなｻｲ❗️❗️❗️❗️✌️👮🔫",
+					BeginningMessageType, RuleNumType, TargetNameType),
+
+				toTemplate(
+					"%sそのツイートはツイッター保護法第%s条🌟"+
+						"「%sで偽スタバなうツイをしてはいけない❗️😡👊🏻」"+
+						"に違反しているゾ😤😤😤💢💢💢！！！！！"+
+						"ぃますぐ😩😩😩そのツイートを削除しなさい💢💢💢！！！😇😇😇",
+					BeginningMessageType, RuleNumType, TargetNameType),
+
+				toTemplate(
+					"ピピーッ❗そのツイートゎ☆Twitterスタバ部のオキテ第%s条・"+
+						"「%sのツイートをスタバなうツイート💕しナイ❗❗」"+
+						"に違反してるゾ❗ただちに消しナさぃ❗", RuleNumType, TargetNameType),
+			},
+			Constraints: map[string]string{
+				ClassType + "/":      ClassRamenValue + "|" + ClassOtherValue,
+				ConfidenceType + "/": ConfidenceHighValue + "|" + ConfidenceMediumValue,
+			},
+		},
+		{
+			Type: RootType,
+			Templates: []string{
+				toTemplate("%sアナタのツイート💕は❌スタバ法❌第%s条🙋"+
+					"「ラーメン...?に似た何かしらをスタバなうツイート💕してゎイケナイ❗️」"+
+					"に違反しています...多分❗気が向いたら消しなｻｲ️✌️👮🔫",
+					BeginningMessageType, RuleNumType),
+				toTemplate("%sそのツイートはツイッター保護法第%s条🌟"+
+					"「ラーメン...?のようなアレで偽スタバなうツイをしてはいけない❗️😡👊🏻」"+
+					"に違反している....ような気がする🤔🤔🤔"+
+					"お時間がある時で結構ですのでそのツイートを削除しなさぃ！！！😇😇😇",
+					BeginningMessageType, RuleNumType,
+				),
+				toTemplate("ピピーッ❗そのツイートゎ☆Twitterスタバ部のオキテ第%s条・"+
+					"「ラーメン...?いやつけ麺...?なんかその辺のツイートをスタバなうツイート💕しナイ❗❗」"+
+					"に違反してる予感がぁりマス❗お手すきの際に消しナさぃ❗", RuleNumType,
+				),
+			},
+			Constraints: map[string]string{
+				ClassType:      ClassRamenValue,
+				ConfidenceType: ConfidenceLowValue,
+			},
+		},
+		{
+			Type: RootType,
+			Templates: []string{toTemplate("%sアナタのツイート💕は❌スタバ法❌第%s条🙋\n"+
+				"「スタバぢゃないツイートをスタバなうツイート💕してゎイケナイ❗️」\n"+
+				"に違反しています😡今スグ消しなｻｲ❗️❗️❗️❗️✌️👮🔫\n", BeginningMessageType, RuleNumType)},
+			Constraints: map[string]string{
+				ClassType:            ClassOtherValue,
 				ConfidenceType + "/": ConfidenceHighValue + "|" + ConfidenceMediumValue},
+		},
+		{
+			Type:      RootType,
+			Templates: []string{"...何?何これ?スタバではないと思うけども...いやマジで何?"},
+			Constraints: map[string]string{
+				ClassType:      ClassOtherValue,
+				ConfidenceType: ConfidenceLowValue,
+			},
 		},
 		{
 			Type: RootType,
 			Templates: []string{toTemplate(
 				`{"class": "%s", "confidence": "%s"}`, ClassType, ConfidenceType)},
 			Constraints: map[string]string{DebugType + ":1": DebugOnValue},
+		},
+		{
+			Type: BeginningMessageType,
+			Templates: []string{
+				"ピピーッ❗️🔔⚡️スタバ警察です❗️👊👮❗️\n",
+				"ピピーっ！👮👮スタバ警察です🚨🚨🚨🙅🙅🙅🙅\n",
+			},
 		},
 		{
 			Type: TweetCheckType,
@@ -126,265 +201,54 @@ func getMessagenDefinitions() []*messagen.Definition {
 		{Type: GoodEmojiType, Templates: []string{"😆", "😂"}},
 		{
 			Type:        SutabaDescriptionType,
-			Templates:   []string{"完全に", "間違いなく"},
+			Templates:   []string{"完膚無きまでに", "完全に", "どこからどう見ても", "ここ10年で最高の", "間違いなく"},
 			Constraints: map[string]string{ConfidenceType: ConfidenceHighValue},
 		},
 		{
 			Type:        SutabaDescriptionType,
-			Templates:   []string{"おそらく", "多分"},
+			Templates:   []string{"おそらく", "おおむね", "比較的", "おおよそ"},
 			Constraints: map[string]string{ConfidenceType: ConfidenceMediumValue},
+		},
+		{
+			Type:        SutabaDescriptionType,
+			Templates:   []string{"たぶん", "どことなく", "もしかすると"},
+			Constraints: map[string]string{ConfidenceType: ConfidenceLowValue},
+		},
+		{
+			Type:        TargetNameType,
+			Templates:   []string{"ラーメン" + w(RamenSuffixType)},
+			Constraints: map[string]string{ClassType: ClassRamenValue},
+		},
+		{
+			Type:        TargetNameType,
+			Templates:   []string{"スタバぢゃない画像"},
+			Constraints: map[string]string{ClassType: ClassOtherValue},
+		},
+		{
+			Type:        RamenSuffixType,
+			Templates:   []string{""},
+			Constraints: map[string]string{ClassType + "/": ClassSutabaValue + "|" + ClassOtherValue},
+		},
+		{
+			Type:        RamenSuffixType,
+			Templates:   []string{"的なアレ", "みたいな画像", "っぽい画像"},
+			Constraints: map[string]string{ClassType: ClassRamenValue},
 		},
 		{
 			Type: LastMessageType,
 			Templates: []string{"この調子でグッドなスタバツイートを心がけるようにッ❗️👮‍👮‍",
-				"市民の協力に感謝するッッッ👮‍👮‍❗"},
+				"市民の協力に感謝するッッッ👮‍👮‍❗",
+				"グッドなスタバに本官もニッコリ☺️☺️☺️️", "やるじゃん😉😉😉",
+			},
+			Constraints: map[string]string{ClassType: ClassSutabaValue},
+		},
+		{
+			Type: LastMessageType,
+			Templates: []string{"アレ法のアレに違反してるゾ❗ただちに消しナさぃ❗",
+				"ぃますぐ😩😩😩そのツイートを削除しなさい💢💢💢！！！😇😇😇",
+				"今スグ消しなｻｲ❗️❗️❗️❗️✌️👮🔫",
+			},
+			Constraints: map[string]string{ClassType + "/": ClassRamenValue + "|" + ClassOtherValue},
 		},
 	}
-}
-
-func classAndConfidenceToText(className string, confidence float32) string {
-	predStr := ""
-	switch className {
-	case "sutaba":
-		if confidence > 0.8 {
-			predStr = GetRandomSutabaHighConfidenceText()
-		} else if confidence > 0.5 {
-			predStr = GetRandomSutabaMiddleConfidenceText()
-		} else {
-			predStr = GetRandomSutabaLowConfidenceText()
-		}
-	case "ramen":
-		if confidence > 0.8 {
-			predStr = GetRandomRamenHighConfidenceText(confidence)
-		} else if confidence > 0.5 {
-			predStr = GetRandomRamenMiddleConfidenceText(confidence)
-		} else {
-			predStr = GetRandomRamenLowConfidenceText(confidence)
-		}
-	case "other":
-		if confidence > 0.8 {
-			predStr = GetRandomOtherHighConfidenceText(confidence)
-		} else if confidence > 0.5 {
-			predStr = GetRandomOtherMiddleConfidenceText(confidence)
-		} else {
-			predStr = GetRandomOtherLowConfidenceText()
-		}
-	}
-
-	return predStr
-}
-
-func pickRandomStr(texts []string) string {
-	rand.Seed(time.Now().Unix())
-	return texts[rand.Intn(len(texts))]
-}
-
-func GetRandomSutabaHighConfidenceText() string {
-	list := []string{"ムムッこのツイート🤔🤔🤔", "オオッこのツイート🤔🤔🤔", "ヤヤッこのツイート🤔🤔🤔",
-		"アァーーーッ️️❗❗何だこれはーーッ❗️❗"}
-	list1 := []string{"完膚無きまでに", "完全に", "どこからどう見ても", "ここ10年で最高の", "間違いなく"}
-	list2 := []string{"この調子でグッドなスタバツイートを心がけるようにッ❗️👮‍♂👮‍♂️️", "市民の協力に感謝するッッッ👮‍👮‍❗❗"}
-	return fmt.Sprintf("ピピーッ❗️🔔⚡️スタバ警察です❗️👊👮❗️ %s...%sスタバ❗️❗️😂😂😂\n%s",
-		pickRandomStr(list),
-		pickRandomStr(list1),
-		pickRandomStr(list2),
-	)
-}
-
-func GetRandomSutabaMiddleConfidenceText() string {
-	list := []string{"ムムッこのツイート🤔🤔🤔", "オオッこのツイート🤔🤔🤔", "ヤヤッこのツイート🤔🤔🤔",
-		"アァーーーッ️️❗❗何だこれはーーッ❗️❗"}
-	list1 := []string{"おおむね", "比較的", "おおよそ"}
-	list2 := []string{"😉😉😉", "😙😙😙", "☺️☺️☺️", "🤗🤗🤗"}
-	list3 := []string{"グッドなスタバに本官もニッコリ☺️☺️☺️️", "やるじゃん😉😉😉"}
-	return fmt.Sprintf("ピピーッ❗️🔔⚡️スタバ警察です❗️👊👮❗️ %s...%sスタバ%s\n%s",
-		pickRandomStr(list),
-		pickRandomStr(list1),
-		pickRandomStr(list2),
-		pickRandomStr(list3),
-	)
-}
-
-func GetRandomSutabaLowConfidenceText() string {
-	list := []string{"ムムッこのツイート🤔🤔🤔", "オオッこのツイート🤔🤔🤔", "ヤヤッこのツイート🤔🤔🤔",
-		"アァーーーッ️️❗❗何だこれはーーッ❗️❗"}
-	list1 := []string{"たぶん", "どことなく", "もしかすると"}
-	list2 := []string{"😐😐😐", "😑😑😑"}
-	list3 := []string{"ほのかなスタバみを感じる...!", "自信ないけど..おじさん最近目が遠くてな~~~👮‍♀️💦"}
-	return fmt.Sprintf("ピピーッ❗️🔔⚡️スタバ警察です❗️👊👮❗️ %s...%s...スタバ......???%s\n%s",
-		pickRandomStr(list),
-		pickRandomStr(list1),
-		pickRandomStr(list2),
-		pickRandomStr(list3),
-	)
-}
-
-func GetRandomRamenHighConfidenceText(confidence float32) string {
-	list0 := []string{"ムムッこのツイート🤔🤔🤔", "オオッこのツイート🤔🤔🤔", "ヤヤッこのツイート🤔🤔🤔",
-		"アァーーーッ️️❗❗何だこれはーーッ❗️❗"}
-	list1 := []string{"完膚無きまでに", "完全に", "どこからどう見ても", "間違いなく"}
-	list2 := []string{
-		"アレ法のアレに違反してるゾ❗ただちに消しナさぃ❗",
-		"ぃますぐ😩😩😩そのツイートを削除しなさい💢💢💢！！！😇😇😇",
-		"今スグ消しなｻｲ❗️❗️❗️❗️✌️👮🔫"}
-	elm := fmt.Sprintf("%s...%sラーメン❗️❗️😂😂😂\nズルズルズルズル❗❗️❗️❗%s",
-		pickRandomStr(list0),
-		pickRandomStr(list1),
-		pickRandomStr(list2),
-	)
-	list := []string{
-		"ピピーッ❗️🔔⚡️スタバ警察です❗️👊👮❗️" +
-			"アナタのツイート💕は❌スタバ法❌第" +
-			fmt.Sprintf("%4.f", confidence*1000) + "条🙋" +
-			"「ラーメンをスタバなうツイート💕してゎイケナイ❗️」" +
-			"に違反しています😡今スグ消しなｻｲ❗️❗️❗️❗️✌️👮🔫",
-		"ピピーっ！👮👮スタバ警察です🚨🚨🚨🙅🙅🙅🙅" +
-			"そのツイートはツイッター保護法第" +
-			fmt.Sprintf("%4.f", confidence*1000) + "条🌟" +
-			"「ラーメンで偽スタバなうツイをしてはいけない❗️😡👊🏻」" +
-			"に違反しているゾ😤😤😤💢💢💢！！！！！" +
-			"ぃますぐ😩😩😩そのツイートを削除しなさい💢💢💢！！！😇😇😇",
-		"ピピーッ❗そのツイートゎ☆" +
-			"Twitterスタバ部のオキテ第" +
-			fmt.Sprintf("%4.f", confidence*1000) + "条・" +
-			"「ラーメンのツイートをスタバなうツイート💕しナイ❗❗」" +
-			"に違反してるゾ❗ただちに消しナさぃ❗",
-		elm, elm, elm,
-	}
-	return pickRandomStr(list)
-}
-
-func GetRandomRamenMiddleConfidenceText(confidence float32) string {
-	list0 := []string{"ムムッこのツイート🤔🤔🤔", "オオッこのツイート🤔🤔🤔", "ヤヤッこのツイート🤔🤔🤔",
-		"アァーーーッ️️❗❗何だこれはーーッ❗️❗"}
-	list1 := []string{"おおむね", "比較的", "おおよそ"}
-	list2 := []string{
-		"アレ法のアレに違反してるゾ❗ただちに消しナさぃ❗",
-		"ぃますぐ😩😩😩そのツイートを削除しなさい💢💢💢！！！😇😇😇",
-		"今スグ消しなｻｲ❗️❗️❗️❗️✌️👮🔫"}
-	elm := fmt.Sprintf("%s...%sラーメン❗️❗️😂😂😂\nズルズルズルズル❗❗️❗️❗%s",
-		pickRandomStr(list0),
-		pickRandomStr(list1),
-		pickRandomStr(list2),
-	)
-	list := []string{
-		"ピピーッ❗️🔔⚡️スタバ警察です❗️👊👮❗️" +
-			"アナタのツイート💕は❌スタバ法❌第" +
-			fmt.Sprintf("%4.f", confidence*1000) + "条🙋" +
-			"「ラーメン的なアレをスタバなうツイート💕してゎイケナイ❗️」" +
-			"に違反しています😡今スグ消しなｻｲ❗️❗️❗️❗️✌️👮🔫",
-		"ピピーっ！👮👮スタバ警察です🚨🚨🚨🙅🙅🙅🙅" +
-			"そのツイートはツイッター保護法第" +
-			fmt.Sprintf("%4.f", confidence*1000) + "条🌟" +
-			"「ラーメンみたいな画像で偽スタバなうツイをしてはいけない❗️😡👊🏻」" +
-			"に違反しているゾ😤😤😤💢💢💢！！！！！" +
-			"ぃますぐ😩😩😩そのツイートを削除しなさい💢💢💢！！！😇😇😇",
-		"ピピーッ❗そのツイートゎ☆" +
-			"Twitterスタバ部のオキテ第" +
-			fmt.Sprintf("%4.f", confidence*1000) + "条・" +
-			"「ラーメンっぽい画像をスタバなうツイート💕しナイ❗❗」" +
-			"に違反してるゾ❗ただちに消しナさぃ❗",
-		elm, elm, elm,
-	}
-	return pickRandomStr(list)
-}
-
-func GetRandomRamenLowConfidenceText(confidence float32) string {
-	list0 := []string{"ムムッこのツイート🤔🤔🤔", "オオッこのツイート🤔🤔🤔", "ヤヤッこのツイート🤔🤔🤔",
-		"アァーーーッ️️❗❗何だこれはーーッ❗️❗"}
-	list1 := []string{"たぶん", "どことなく", "もしかすると"}
-	list2 := []string{"😐😐😐", "😑😑😑"}
-	list3 := []string{
-		"そのうち消しナさぃ❗",
-		"近いうちにそのツイートを削除しなさい😇😇😇",
-		"今スグとは言わないので消しなｻｲ❗️❗️❗️❗️✌️👮🔫"}
-	elm := fmt.Sprintf("%s...%s...ラーメン......???%s%s",
-		pickRandomStr(list0),
-		pickRandomStr(list1),
-		pickRandomStr(list2),
-		pickRandomStr(list3),
-	)
-	list := []string{
-		"ピピーッ❗️🔔⚡️スタバ警察です❗️👊👮❗️" +
-			"アナタのツイート💕は❌スタバ法❌第" +
-			fmt.Sprintf("%4.f", confidence*1000) + "条🙋" +
-			"「ラーメン...?に似た何かしらをスタバなうツイート💕してゎイケナイ❗️」" +
-			"に違反しています...多分❗気が向いたら消しなｻｲ️✌️👮🔫",
-		"ピピーっ！👮👮スタバ警察です🚨🚨🚨🙅🙅🙅🙅" +
-			"そのツイートはツイッター保護法第" +
-			fmt.Sprintf("%4.f", confidence*1000) + "条🌟" +
-			"「ラーメン...?のようなアレで偽スタバなうツイをしてはいけない❗️😡👊🏻」" +
-			"に違反している....ような気がする🤔🤔🤔" +
-			"お時間がある時で結構ですのでそのツイートを削除しなさい！！！😇😇😇",
-		"ピピーッ❗そのツイートゎ☆" +
-			"Twitterスタバ部のオキテ第" +
-			fmt.Sprintf("%4.f", confidence*1000) + "条・" +
-			"「ラーメン...?いやつけ麺...?なんかその辺の...のツイートをスタバなうツイート💕しナイ❗❗」" +
-			"に違反してる..という気がしないでもないゾ❗お手すきの際に消しナさぃ❗",
-		elm, elm, elm,
-	}
-	return pickRandomStr(list)
-}
-
-func GetRandomOtherHighConfidenceText(confidence float32) string {
-	rand.Seed(time.Now().Unix())
-	if rand.Intn(20) < 2 {
-		return "ピピーッ❗️🔔⚡️スタバ警察です❗️👊👮❗" +
-			"アナタのツイート💕は❌スタバ法❌第..." +
-			"ピピーッ❗️❗🔔⚡️スタバ警察です❗️👊👮❗アナ..." +
-			"ピピーッ❗️❗ピピピピーッッ❗❗❗❗🔔⚡️スタ..." +
-			"ピピピッピピピピーッ❗️❗❗❗ピピピピーッッッッ❗❗❗❗❗️"
-	}
-	list := []string{
-		"ピピーッ❗️🔔⚡️スタバ警察です❗️👊👮❗️" +
-			"アナタのツイート💕は❌スタバ法❌第" +
-			fmt.Sprintf("%4.f", confidence*1000) + "条🙋" +
-			"「スタバぢゃないツイートをスタバなうツイート💕してゎイケナイ❗️」" +
-			"に違反しています😡今スグ消しなｻｲ❗️❗️❗️❗️✌️👮🔫",
-		"ピピーっ！👮👮スタバ警察です🚨🚨🚨🙅🙅🙅🙅" +
-			"そのツイートはツイッター保護法第" +
-			fmt.Sprintf("%4.f", confidence*1000) + "条🌟" +
-			"「偽スタバなうツイをしてはいけない❗️😡👊🏻」" +
-			"に違反しているゾ😤😤😤💢💢💢！！！！！" +
-			"ぃますぐ😩😩😩そのツイートを削除しなさい💢💢💢！！！😇😇😇",
-		"ピピーッ❗そのツイートゎ☆" +
-			"Twitterスタバ部のオキテ第" +
-			fmt.Sprintf("%4.f", confidence*1000) + "条・" +
-			"「スタバぢゃないツイートをスタバなうツイート💕しナイ❗❗」" +
-			"に違反してるゾ❗ただちに消しナさぃ❗",
-	}
-	return pickRandomStr(list)
-}
-
-func GetRandomOtherMiddleConfidenceText(confidence float32) string {
-	list := []string{"おおむね", "比較的", "おおよそ", "だいぶ", "かなり"}
-	templates := []string{
-		"ピピーッ❗️🔔⚡️スタバ警察です❗️👊👮❗️" +
-			"アナタのツイート💕は❌スタバ法❌第" +
-			fmt.Sprintf("%4.f", confidence*1000) + "条🙋" +
-			"「%sスタバぢゃないツイートをスタバなうツイート💕してゎイケナイ❗️」" +
-			"に違反しています😡今スグ消しなｻｲ❗️❗️❗️❗️✌️👮🔫",
-		"ピピーっ！👮👮スタバ警察です🚨🚨🚨🙅🙅🙅🙅" +
-			"そのツイートはツイッター保護法第" +
-			fmt.Sprintf("%4.f", confidence*1000) + "条🌟" +
-			"「%s偽スタバなうツイをしてはいけない❗️😡👊🏻」" +
-			"に違反しているゾ😤😤😤💢💢💢！！！！！" +
-			"ぃますぐ😩😩😩そのツイートを削除しなさい💢💢💢！！！😇😇😇",
-		"ピピーッ❗そのツイートゎ☆" +
-			"Twitterスタバ部のオキテ第" +
-			fmt.Sprintf("%4.f", confidence*1000) + "条・" +
-			"「%sスタバぢゃないツイートをスタバなうツイート💕しナイ❗❗」" +
-			"に違反してるゾ❗ただちに消しナさぃ❗",
-	}
-	return fmt.Sprintf(pickRandomStr(templates), pickRandomStr(list))
-}
-
-func GetRandomOtherLowConfidenceText() string {
-	list := []string{
-		"...何?何これ?スタバではないと思うけども...いやマジで何?",
-		"...何?何これ?スタバではないと思うけども...いやマジで何?",
-		"...何?何これ?スタバではないと思うけども...いやマジで何?",
-		"はい",
-	}
-	return pickRandomStr(list)
 }
