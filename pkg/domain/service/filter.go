259eef75fd7e08ba873969260d0b27923b89a690
niboshi <niboshiporipori@gmail.com>
2020-10-07 01:48:11 +0900

    Fix IsTargetMessage (#62)
    スタバツイートを引用リツイートした自身のツイートに反応してしまうバグを修正

--- pkg/domain/service/filter.go
+++ pkg/domain/service/filter.go
@@ -2,13 +2,28 @@ package domain
 
 import "github.com/mpppk/sutaba-server/pkg/domain/model"
 
-func IsTargetMessage(user *model.User, message *model.Message, keyword string) string {
-	if message.MediaNum == 0 {
-		return "message is ignored because it has no media"
+// IsTargetMessage checks if the given message is a target.
+func IsTargetMessage(user *model.User, message *model.Message) (bool, bool, string) {
+	if user.IsOwnMessage(message) {
+		return false, false, "message is ignored because it is sent by bot"
 	}
 
-	if user.IsOwnMessage(message) {
-		return "message is ignored because it is sent by bot"
+	if message.ReferencedMessage != nil && user.IsOwnMessage(message.ReferencedMessage) {
+		return false, false, "message is ignored because it refer bot message"
 	}
-	return ""
+
+	// メッセージが画像を含む場合は、referされたメッセージは無視して判定
+	if message.MediaNum != 0 {
+		return true, false, ""
+	}
+
+	if message.ReferencedMessage == nil {
+		return false, false, "message is ignored because it has no media and does not have referenced message"
+	}
+
+	if message.ReferencedMessage.MediaNum != 0 {
+		return false, true, ""
+	}
+
+	return false, false, "message is ignored because referred message has no media"
 }
